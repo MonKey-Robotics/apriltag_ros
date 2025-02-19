2b0d45722fc4858c74a2076bd713bd41b15984c3
LocoHao <tkhao96@gmail.com>
2025-02-19 14:41:17 +0800

    fixed launch file

--- apriltag_ros/launch/apriltag_ros.launch.py
+++ apriltag_ros/launch/apriltag_ros.launch.py
@@ -21,7 +21,8 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+import os
+import ament_index_python.packages
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
